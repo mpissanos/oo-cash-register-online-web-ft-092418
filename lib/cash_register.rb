9fcc4c6aff65ef8e504ddbba566d1edfae9134c6
Manny Pissanos <mpissano@gmail.com>
2018-10-04 14:05:30 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,32 @@
-
+class CashRegister
+  
+  attr_accessor
+  
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+  
+  def self.total
+    @total
+  end
+  
+  def add_item(title, price, quantity = 0)
+  
+  end
+  
+  def apply_discount
+  
+  end
+  
+  def items
+    
+  end
+  
+  def void_last_transaction
+    
+  end
+    
+    
+end
