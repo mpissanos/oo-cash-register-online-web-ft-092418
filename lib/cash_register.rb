dab5fa4f2442e98c1a40108aa2c663eabc2a70dc
Manny Pissanos <mpissano@gmail.com>
2018-10-07 12:59:31 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,7 +14,7 @@ class CashRegister
     self.total += price * quantity
     self.last_transaction = price*quantity
     quantity.times do
-    @items << title
+      @items << title
     end
   end
   
